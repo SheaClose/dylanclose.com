74f77f02d5dc5cfbdcdc51055303c5d83180eea3
sheaclose <sheaclose@gmail.com>
2021-02-04 15:28:47 -0600

    update menu

--- pages/Menu.vue
+++ pages/Menu.vue
@@ -1,10 +1,27 @@
 <template>
-  <div>Menu</div>
+  <div>
+    <div class="p-col-11 p-md-10 p-lg-9">
+      <script src="https://square.site/appointments/buyer/widget/cd228kxu1umeet/LK3MWY9F8QC96.js"></script>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style>
+<style lang="scss">
+.content-body {
+  background-image: url("/main.jpg");
+  background-size: cover;
+  background-position: center center;
+  background-repeat: no-repeat;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  iframe {
+    height: 75vh;
+  }
+}
+
 </style>
